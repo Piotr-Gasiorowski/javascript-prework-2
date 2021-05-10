112f29374d02d9c56d0d85446aee7b67d4e2543c
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-05-10 11:19:39 +0200

    Początek

---
+++ js/script.js
@@ -0,0 +1,47 @@
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+else if(playerInput == '2'){
+	playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+else if(playerInput == '3') {
+	playerMove = 'papier';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+else if(playerInput => '4') {
+	playerMove = 'nieznany ruch';
+}
+
+printMessage('Nieznany ruch');
+
+else if(playerMove == computerMove) {
+	printMessage('!!! REMIS !!!')
+}
+	
+if(computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}else if(computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Komputer wygrywa!');
+}else if(computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Komputer wygrywa!');
+}else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}else if(computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Komputer wygrywa!');
+}
